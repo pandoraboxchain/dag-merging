9f1d123fc3a4b68bc334ebffca54b91f683b59f7
Andrey Sobol <sobol@pandora.foundation>
2018-09-15 23:47:44 +0300

    add Block

---
+++ main.py
@@ -0,0 +1,8 @@
+class Block:
+
+    def __init__(self, timeslot, identifier, is_empty, is_immutable):
+        self.timeslot = timeslot
+        self.identifier = identifier
+        self.is_empty = is_empty
+        self.is_immutable = is_immutable
+
