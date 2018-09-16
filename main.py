9cf42dff66cc04ac5748517f6b6514a3e62c5693
Andrey Sobol <sobol@pandora.foundation>
2018-09-17 02:40:41 +0300

    full merge

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ class Chain(list):
         mx = max(len(self), len(another))
         return {
             0: self[dpoint:m],
-            1: self[dpoint:m],
+            1: another[dpoint:m],
         }
 
     def get_merging_point(self):
@@ -49,6 +49,12 @@ class Chain(list):
             return res[0]
         else:
             return False
+    
+    def get_all_immutable(self):
+        return [item for item in self if item.is_immutable]
+
+    def get_all_mutable(self):
+        return [item for item in self if not item.is_immutable]
 
 def merge(chains):
     sizes = [chain.get_chain_size() for chain in chains]
@@ -68,5 +74,29 @@ def merge(chains):
 
     active = deterministic_ordering[0]
     mp = active.get_merging_point()
+    # TODO: check list or Chain
+    merged_chain = active[:mp]
+
+    for doi in deterministic_ordering[1:]:
+        diff = active.get_diff(chains[doi])[1]
+        # TODO: check list or Chain
+        diffchain = chains[doi][diff:]
+        im_chain = diffchain.get_all_immutable()
+        if im_chain:
+            for im in im_chain:
+                if im.is_empty:
+                    if merged_chain.find_block_by_identifier(im.identifier):
+                        merged_chain.append(im)
+    
+    for doi in deterministic_ordering:
+        diff = active.get_diff(chains[doi])[1]
+        # TODO: check list or Chain
+        diffchain = chains[doi][diff:]
+        m_chain = diffchain.get_all_mutable()
+        if m_chain:
+            for m in m_chain:
+                if m.is_empty:
+                    if merged_chain.find_block_by_identifier(m.identifier):
+                        merged_chain.append(m)
 
     return deterministic_ordering
