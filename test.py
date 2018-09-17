258317d2988e6d7acc4e08f82ec06d7eff46d88d
Andrey Sobol <sobol@pandora.foundation>
2018-09-17 03:08:59 +0300

    fix naming

--- test.py
+++ test.py
@@ -2,9 +2,9 @@ import unittest
 
 from main import Block, Chain, merge
 
-class SimpleTest(unittest.TestCase):
+class TestSimple(unittest.TestCase):
     
-    def simple_test(self):
+    def test_simple(self):
         zero = Block(0, 0, False, True)
         first = Block(1, 1, False, True)
         second = Block(2, 2, False, False)
@@ -14,5 +14,6 @@ class SimpleTest(unittest.TestCase):
         first_chain = Chain([zero, third])
 
         res = merge(zero_chain, first_chain)
+        self.assertTrue(res)
 
 unittest.main()
